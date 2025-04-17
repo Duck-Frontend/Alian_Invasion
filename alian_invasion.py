6063251005b55b9b01c7592fae841e677f6340cc
Anton Stadolnik <stadolnikanton1@gmail.com>
2025-04-17 19:46:19 +0300

    Отоброжение корабля в окне игы и рефакторинг функции run_game

--- alian_invasion.py
+++ alian_invasion.py
@@ -1,6 +1,7 @@
 import sys
-import pygame # type: ignore
+import pygame
 from setting import Settign
+from ship import Ship
 
 
 class AlianInvasion:
@@ -13,18 +14,28 @@ class AlianInvasion:
             (self.setting.screen_width, self.setting.screen_height))
         self.clock = pygame.time.Clock()
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
 
     def run_game(self):
         """Запускает основной цикл игры"""
         while True:
             # Отслеживание событий клавиатуры и мыши
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            self.screen.fill(self.setting.bg_color)
-            # Отображение последнего прорисовоного экрана
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
             self.clock.tick(60)
+
+    def _check_events(self):
+        """Обрабатывает нажатие с клавиатуры и мыши"""
+        for event in pygame.event.get():
+            # print(event)
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def _update_screen(self):
+        """Обновляет изображение на экране и отображает новый экран"""
+        self.screen.fill(self.setting.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
